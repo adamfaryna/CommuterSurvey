a164244bcd6374d7bade0509675bf9464b1214a8
Adam Faryna <adamfaryna@gmail.com>
2016-01-15 20:14:18 +0100

    prepare angular folders structure

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,65 @@
+'use strict';
+
 var gulp         = require('gulp');
+var plugins = require('gulp-load-plugins')();
 var browserSync  = require('browser-sync').create();
-var less         = require('gulp-less');
-var autoprefixer = require('gulp-autoprefixer');
+
+var paths = {
+  lint: ['*.js', './public/**/*.js'],
+  watch: ['*.js', './public/**', '!bower_components'],
+  tests: ['./test/**/*.js'],
+  source: ['public/app/**/*.js']
+};
+
+gulp.task('nodemon', function (cb) {
+	var started = false;
+	
+	return plugins.nodemon({
+		script: 'app.js'
+
+	}).on('start', function () {
+		// to avoid nodemon being started multiple times
+		if (!started) {
+			cb();
+			started = true; 
+		} 
+	});
+});
+
+gulp.task('lint', function () {
+  return gulp.src(paths.lint)
+    .pipe(plugins.jshint('.jshintrc'))
+    // .pipe(plugins.plumber(plumberConf))
+    // .pipe(plugins.jscs())
+    .pipe(plugins.jshint.reporter('jshint-stylish'));
+});
+
+
+// Compile less into CSS & auto-inject into browsers
+gulp.task('less', function() {
+  return gulp.src('less/*.scss')
+    .pipe(plugins.less())
+    .pipe(plugins.autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('public/build'))
+    .pipe(browserSync.stream());
+});
+
+// gulp.task('minify', ['less'], function() {
+//   return gulp.src('less/*.scss')
+//     .pipe(less())
+//     .pipe(autoprefixer({
+//       browsers: ['last 2 versions'],
+//       cascade: false
+//     }))
+//     .pipe(gulp.dest('public/build'))
+//     .pipe(browserSync.stream());
+// });
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['less'], function() {
+gulp.task('serve', ['less', 'nodemon'], function() {
   browserSync.init({
     browser: ['firefox'],
     server: './public',
@@ -15,16 +70,6 @@ gulp.task('serve', ['less'], function() {
   gulp.watch('public/index.html').on('change', browserSync.reload);
 });
 
-// Compile less into CSS & auto-inject into browsers
-gulp.task('less', function() {
-  return gulp.src('less/*.scss')
-    .pipe(less())
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest('public/build'))
-    .pipe(browserSync.stream());
-});
+gulp.task('start', ['less'], function () {});
 
 gulp.task('default', ['serve']);
